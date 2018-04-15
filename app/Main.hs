b1bedbe84874aee320c1c15421688eeeec12fee7
Ludvig Sundström <lud.sund@gmail.com>
2018-04-15 19:37:20 +0200

    Add serverstate

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,37 @@ module Main where
 
 -- import Lib
 
-import Web.Spock
-import Web.Spock.Config
+import           Control.Monad          (forM_)
+import           Control.Monad.IO.Class (liftIO)
+import           Data.IORef
+import           Data.Text              (Text)
+import           Lucid
+import           Web.Spock
+import           Web.Spock.Config
+import           Web.Spock.Lucid        (lucid)
 
-type Server a = SpockM () () () a
+data Note = Note { author :: Text, contents :: Text }
+
+newtype ServerState = ServerState { notes :: IORef [Note] }
+
+type Server a = SpockM () () ServerState a
 
 app :: Server ()
-app = get root (text "Hello!")
+app = get root $ do
+    notes' <- getState >>= (liftIO . readIORef . notes)
+    lucid $ do
+        h1_ "Notes"
+        ul_ $ forM_ notes' $ \note -> li_ $ do
+            toHtml (author note)
+            ": "
+            toHtml (contents note)
 
 main :: IO ()
 main = do
-    cfg <- defaultSpockCfg () PCNoDatabase ()
+    st <- ServerState <$>
+            newIORef
+            [ Note "Alice" "Must not forget to walk the dog"
+            , Note "Bob" "Must eat pizza" ]
+    cfg <- defaultSpockCfg () PCNoDatabase st
     runSpock 8080 (spock cfg app)
 
