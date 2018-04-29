8caed5e964240ca8330466ffbe259f535dd4ba9d
Ludvig Sundström <lud.sund@gmail.com>
2018-04-29 14:25:57 +0200

    New module for CSS classes

---
+++ src/Calendar/CssClasses.hs
@@ -0,0 +1,27 @@
+module Calendar.CssClasses where
+
+import Protolude
+import           Data.Time.LocalTime
+
+import           Calendar.Data.Entry
+
+entry :: Entry -> TimeOfDay -> Text
+entry e _
+    | _done e             = "done mui--divider-bottom"
+    | otherwise           = ""
+
+
+button :: Text
+button = "submit mui-btn mui-btn--primary"
+
+
+form :: Text
+form = "mui-form--inline"
+
+
+time :: Text
+time = "time mui-textfield"
+
+
+desc :: Text
+desc = "desc mui-textfield"
--- src/Calendar/ViewComponents.hs
+++ src/Calendar/ViewComponents.hs
@@ -6,6 +6,7 @@ import           Data.Time.LocalTime
 import           Lucid
 import           Protolude
 
+import qualified Calendar.CssClasses as C
 import           Calendar.Data.Day
 import           Calendar.Data.Entry
 
@@ -16,22 +17,6 @@ isPast (TimeOfDay h m _) (TimeOfDay h' m' _)
     | h' == h && m' < m = True
     | otherwise         = False
 
--------------------------------------------------------------------------------
--- Classes
-
-entryClasses :: Entry -> TimeOfDay -> [Attribute]
-entryClasses e _
-    | _done e             = [class_ "done mui--divider-bottom"]
-    -- | ct `isPast` _time e = [class_ "past"]
-    | otherwise           = [class_ "mui-form--inline"]
-
-
-textClasses :: [Attribute]
-textClasses = [class_ "mui-textfield"]
-
-buttonClasses :: [Attribute]
-buttonClasses = [class_ "mui-btn mui-btn--small"]
-
 
 -------------------------------------------------------------------------------
 -- Public API
@@ -41,38 +26,37 @@ entry d e
     | _done e = div_ (toHtml (show e :: Text))
     | otherwise = do
         span_ $
-            form_ [method_ "post", action_ "update"] $ do
+            form_ [class_ C.form, method_ "post", action_ "update"] $ do
                 input_ [type_ "hidden", name_ "id", value_ (show (_id e))]
                 input_ [type_ "hidden", name_ "day", value_ (show d)]
                 input_ [type_ "hidden", name_ "done", value_ (show (_done e))]
-                div_ [class_ "time mui-textfield"] $
+                div_ [class_ C.time] $
                     input_ [type_ "text", name_ "time", value_ $ T.take 5 (show $ _time e)]
-                div_ [class_ "desc mui-textfield"] $
+                div_ [class_ C.desc] $
                     input_ [type_ "text", name_ "desc", value_ (_desc e :: Text)]
-                input_ [class_ "mui-btn mui-btn--primary", type_ "submit", value_ "update"]
+                input_ [class_ C.button, type_ "submit", value_ "update"]
 
         span_ $
-            form_ [method_ "post", action_ "done"] $ do
+            form_ [class_ C.form, method_ "post", action_ "done"] $ do
                 input_ [type_ "hidden", name_ "id", value_ (show (_id e))]
-                input_ [class_ "mui-btn mui-btn--primary", type_ "submit", value_ "done"]
+                input_ [class_ C.button, type_ "submit", value_ "done"]
 
         span_ $
-            form_ [method_ "post", action_ "delete"] $ do
+            form_ [class_ C.form, method_ "post", action_ "delete"] $ do
                 input_ [type_ "hidden", name_ "id", value_ (show (_id e))]
-                input_ [class_ "mui-btn mui-btn--primary", type_ "submit", value_ "delete"]
+                input_ [class_ C.button, type_ "submit", value_ "delete"]
 
 
 newEntry :: Int -> HtmlT Identity ()
 newEntry id =
-    form_ [class_ "newform mui-form--inline", method_ "post", action_ "add"] $
-        div_ [class_ "newcontent"] $ do
-            input_ [type_ "hidden", name_ "id", value_ (show id)]
-            div_ [class_ "time mui-textfield"] $
-                input_ [type_ "text", name_ "time", placeholder_ "hh:mm"]
-            div_ [class_ "desc mui-textfield"] $
-                input_ [type_ "text", name_ "desc", placeholder_ "Description"]
-            input_
-                [class_ "mui-btn mui-btn--primary", type_ "submit", value_ "Add Entry"]
+    form_ [class_ C.form, method_ "post", action_ "add"] $ do
+        input_ [type_ "hidden", name_ "id", value_ (show id)]
+        div_ [class_ C.time] $
+            input_ [type_ "text", name_ "time", placeholder_ "hh:mm"]
+        div_ [class_ C.desc] $
+            input_ [type_ "text", name_ "desc", placeholder_ "Description"]
+        input_
+            [class_ C.button, type_ "submit", value_ "Add Entry"]
 
 
 day :: Day -> TimeOfDay -> HtmlT Identity ()
@@ -81,4 +65,4 @@ day (Day id d es) ct =
         div_ [class_ "date"] $ h2_ $ toHtml (dayFormat d)
         div_ [class_ "new"] $ newEntry id
         div_ [class_ "entries"] $
-            ul_ $ forM_ (sortEntries es) (\e -> li_ (entryClasses e ct) $ entry d e)
+            ul_ $ forM_ (sortEntries es) (\e -> li_ [class_ $ C.entry e ct] $ entry d e)
