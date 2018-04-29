e65e4e44b0c614997aabb48b316baea1b64dbfab
Ludvig Sundström <lud.sund@gmail.com>
2018-04-29 11:13:00 +0200

    Styling

--- src/Calendar/ViewComponents.hs
+++ src/Calendar/ViewComponents.hs
@@ -38,47 +38,46 @@ buttonClasses = [class_ "mui-btn mui-btn--small"]
 entry :: Date -> Entry -> HtmlT Identity ()
 entry d e
     | _done e = div_ (toHtml (show e :: Text))
-    | otherwise =
-        div_  $ do
-            div_ [class_ "left"] $
-                form_ [method_ "post", action_ "update"] $
-                    div_ $ do
-                        input_ [type_ "hidden", name_ "id", value_ (show (_id e))]
-                        input_ [type_ "hidden", name_ "day", value_ (show d)]
-                        input_ [type_ "hidden", name_ "done", value_ (show (_done e))]
-                        input_ [class_ "time", type_ "text", name_ "time", value_ (show $ _time e)]
-                        input_ [class_ "desc", type_ "text", name_ "desc", value_ (_desc e :: Text)]
-                        input_ [type_ "submit", value_ "update"]
+    | otherwise = do
+        span_ $
+            form_ [method_ "post", action_ "update"] $ do
+                input_ [type_ "hidden", name_ "id", value_ (show (_id e))]
+                input_ [type_ "hidden", name_ "day", value_ (show d)]
+                input_ [type_ "hidden", name_ "done", value_ (show (_done e))]
+                div_ [class_ "time mui-textfield"] $
+                    input_ [type_ "text", name_ "time", value_ (show $ _time e)]
+                div_ [class_ "desc mui-textfield"] $
+                    input_ [type_ "text", name_ "desc", value_ (_desc e :: Text)]
+                input_ [class_ "mui-btn mui-btn--primary", type_ "submit", value_ "update"]
 
-            div_ [class_ "left"] $
-                form_ [method_ "post", action_ "done"] $ do
-                    input_ [type_ "hidden", name_ "id", value_ (show (_id e))]
-                    input_ [type_ "submit", value_ "done"]
+        span_ $
+            form_ [method_ "post", action_ "done"] $ do
+                input_ [type_ "hidden", name_ "id", value_ (show (_id e))]
+                input_ [class_ "mui-btn mui-btn--primary", type_ "submit", value_ "done"]
 
-            div_  $
-                form_ [method_ "post", action_ "delete"] $ do
-                    input_ [type_ "hidden", name_ "id", value_ (show (_id e))]
-                    input_ [type_ "submit", value_ "delete"]
+        span_ $
+            form_ [method_ "post", action_ "delete"] $ do
+                input_ [type_ "hidden", name_ "id", value_ (show (_id e))]
+                input_ [class_ "mui-btn mui-btn--primary", type_ "submit", value_ "delete"]
 
 
 newEntry :: Int -> HtmlT Identity ()
 newEntry id =
-    form_ [method_ "post", action_ "add"] $ do
-        input_ [type_ "hidden", name_ "id", value_ (show id)]
-        input_ [class_ "time", type_ "text", name_ "time", value_ "00:00"]
-        textarea_ [name_ "desc"] ""
-        input_
-            [type_ "submit", value_ "Add Entry"]
+    form_ [class_ "newform mui-form--inline", method_ "post", action_ "add"] $
+        div_ [class_ "newcontent"] $ do
+            input_ [type_ "hidden", name_ "id", value_ (show id)]
+            div_ [class_ "time mui-textfield"] $
+                input_ [type_ "text", name_ "time", placeholder_ "hh:mm"]
+            div_ [class_ "desc mui-textfield"] $
+                input_ [type_ "text", name_ "desc", placeholder_ "Description"]
+            input_
+                [class_ "mui-btn mui-btn--primary", type_ "submit", value_ "Add Entry"]
 
 
 day :: Day -> TimeOfDay -> HtmlT Identity ()
 day (Day id d es) ct =
     div_ [class_ "day"] $ do
-        h2_ [class_ "date"] $ toHtml (dayFormat d)
+        div_ [class_ "date"] $ h2_ $ toHtml (dayFormat d)
+        div_ [class_ "new"] $ newEntry id
         div_ [class_ "entries"] $
             ul_ $ forM_ (sortEntries es) (\e -> li_ (entryClasses e ct) $ entry d e)
-
-        div_ [class_ "mui-divider"] ""
-
-        div_  [class_ "newentry"] $
-            newEntry id
