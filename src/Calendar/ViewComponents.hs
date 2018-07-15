68c9234a910e6e8433259c9420917377164927c7
Ludvig Sundström <lud.sund@gmail.com>
2018-07-15 09:12:53 +0200

    Show time better

--- src/Calendar/ViewComponents.hs
+++ src/Calendar/ViewComponents.hs
@@ -13,6 +13,11 @@ import           Calendar.Forecast (Weather)
 import qualified Calendar.Forecast as FC
 
 
+showTime :: Maybe TimeOfDay -> Text
+showTime (Just t) = T.take 5 $ show t
+showTime Nothing = ""
+
+
 isPast :: TimeOfDay -> TimeOfDay -> Bool
 isPast (TimeOfDay h m _) (TimeOfDay h' m' _)
     | h' < h            = True
@@ -26,7 +31,7 @@ isPast (TimeOfDay h m _) (TimeOfDay h' m' _)
 entry :: Date -> Entry -> HtmlT Identity ()
 entry d e
     | _done e = do
-        span_ (toHtml (T.take 5 $ show $ _time e :: Text))
+        span_ $ (toHtml . showTime . _time) e
         span_ [class_ "sep-x"] (toHtml ("" :: Text))
         span_ (toHtml (_desc e :: Text))
     | otherwise = do
@@ -36,7 +41,7 @@ entry d e
                 input_ [type_ "hidden", name_ "day", value_ (show d)]
                 input_ [type_ "hidden", name_ "done", value_ (show (_done e))]
                 div_ [class_ C.time] $
-                    input_ [type_ "text", name_ "time", value_ $ T.take 5 (show $ _time e)]
+                    input_ [type_ "text", name_ "time", value_ $ (showTime . _time) e]
                 div_ [class_ C.desc] $
                     input_ [type_ "text", name_ "desc", value_ (_desc e :: Text)]
                 input_ [class_ C.button, type_ "submit", value_ "update"]
@@ -56,9 +61,8 @@ newEntry :: Int -> HtmlT Identity ()
 newEntry id =
     form_ [class_ C.form, method_ "post", action_ "add"] $ do
         input_ [type_ "hidden", name_ "id", value_ (show id)]
-        -- TODO remove this and add with default time
-        div_ [class_ C.time] $
-            input_ [type_ "text", name_ "time", value_ "00:00"]
+        -- div_ [class_ C.time] $
+        --     input_ [type_ "text", name_ "time", value_ "00:00"]
         div_ [class_ C.desc] $
             input_ [type_ "text", name_ "desc", placeholder_ "Description"]
         input_
